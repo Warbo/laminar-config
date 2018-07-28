09c4855a269eff0a4b0f1682cd7d35374bfee875
Chris Warburton <chriswarbo@gmail.com>
2018-07-28 12:42:06 +0100

    Keep going and count up failures

--- default.nix
+++ default.nix
@@ -91,13 +91,23 @@ with rec {
           echo "Finding derivations" 1>&2
           DRVPATHS=$(nix eval --show-trace --raw "$ATTRS")
           echo "Building derivations" 1>&2
+          COUNT=0
+          FAILS=0
           while read -r PAIR
           do
+            COUNT=$(( COUNT + 1 ))
             ATTR=$(echo "$PAIR" | cut -f1)
              DRV=$(echo "$PAIR" | cut -f2)
             echo "Building $ATTR" 1>&2
-            nix-store --show-trace --realise "$DRV"
+            nix-store --show-trace --realise "$DRV" || FAILS=$(( FAILS + 1 ))
           done < <(echo "$DRVPATHS")
+          if [[ "$FAILS" -eq 0 ]]
+          then
+            echo "All $COUNT built successfully" 1>&2
+          else
+            printf '%s/%s builds failed\n' "$FAILS" "$COUNT" 1>&2
+            exit 1
+          fi
         '';
       };
   };
